60bfcf5e9dc979c78f741004af10aa6d72f99cf9
Louis Dutton <louis@dutton.digital>
2026-02-24 11:50:15 +0000

    Standardise build with Nix package
    - Add packages.default output to flake.nix
    - Update workflow to use nix build
    - Add result symlink to .gitignore

--- flake.nix
+++ flake.nix
@@ -1,11 +1,22 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, self, ... }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
     in {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "duttondigital-site";
+          version = "0.1.0";
+          src = self;
+          nativeBuildInputs = [ pkgs.zola ];
+          buildPhase = "zola build";
+          installPhase = "cp -r public $out";
+        };
+      });
+
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           packages = [ pkgs.zola ];
